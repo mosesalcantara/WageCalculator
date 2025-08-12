2479c0af1bcb14697109c29aec43cc142675df61
June Balaibo <junebalaibo03@gmail.com>
2025-08-12 10:24:38 +0800

    splash and fade

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,86 @@
-import { Text, View } from "react-native";
+import React, {useEffect, useRef} from 'react';
+import {Animated, Text, View, Image, StyleSheet} from 'react-native';
+import LoaderImage from '@/assets/images/splash-icon.png'; //The iMage to be displayed
 
-export default function Index() {
+const FadeInView = props => {
+  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 10000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim, // Bind opacity to animated value
+      }}>
+      {props.children}
+    </Animated.View>
+  );
+};
+
+// You can then use your `FadeInView` in place of a `View` in your components:
+export default () => {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this mobile.</Text>
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <FadeInView>
+      <Image source={LoaderImage} style={styles.image} />
+      </FadeInView>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#f8f9fa',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  centeredContainter: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+});
