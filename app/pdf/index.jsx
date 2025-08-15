210518414bbc79c4db658404b080b085d78f877b
June Balaibo <junebalaibo03@gmail.com>
2025-08-15 13:16:53 +0800

    Employee-PDF

---
+++ app/pdf/index.jsx
@@ -0,0 +1,11 @@
+import Pdf from "@/components/pdf/Pdf";
+import NavBar from "@/components/NavBar/NavBar";
+
+export default function PdfPage() {
+  return (
+      <>
+      <NavBar />
+      <Pdf />
+    </>
+    )
+}
