9adfe45d4bf4ad1bfb3286ef1ef6a7f77e2cc6ac
MosesAlcantara <mosesalcantara123@gmail.com>
2025-09-24 08:32:11 +0800

    Reverted changes

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,22 +1,21 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
-import journal from './meta/_journal.json';
-import m0000 from './0000_bouncy_justice.sql';
-import m0001 from './0001_easy_scourge.sql';
-import m0002 from './0002_brief_veda.sql';
-import m0003 from './0003_moaning_khan.sql';
-import m0004 from './0004_unknown_komodo.sql';
-import m0005 from './0005_rich_johnny_blaze.sql';
+import m0000 from "./0000_bouncy_justice.sql";
+import m0001 from "./0001_easy_scourge.sql";
+import m0002 from "./0002_brief_veda.sql";
+import m0003 from "./0003_moaning_khan.sql";
+import m0004 from "./0004_unknown_komodo.sql";
+import m0005 from "./0005_rich_johnny_blaze.sql";
+import journal from "./meta/_journal.json";
 
-  export default {
-    journal,
-    migrations: {
-      m0000,
-m0001,
-m0002,
-m0003,
-m0004,
-m0005
-    }
-  }
-  
+export default {
+  journal,
+  migrations: {
+    m0000,
+    m0001,
+    m0002,
+    m0003,
+    m0004,
+    m0005,
+  },
+};
