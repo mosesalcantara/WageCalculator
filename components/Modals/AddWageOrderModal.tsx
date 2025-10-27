693331abea89eae2f18a5280ab75bd3eb4df26ac
MosesAlcantara <mosesalcantara123@gmail.com>
2025-10-27 09:29:55 +0800

    Converted wage order modals

--- components/Modals/AddWageOrderModal.tsx
+++ components/Modals/AddWageOrderModal.tsx
@@ -1,13 +1,11 @@
 import { wageOrders } from "@/db/schema";
-import {
-  wageOrder as validationSchema,
-  WageOrder as Values,
-} from "@/schemas/globals";
+import { wageOrder as schema, WageOrder as Values } from "@/schemas/globals";
 import { Db } from "@/types/globals";
-import { formatDateValue, toastVisibilityTime } from "@/utils/globals";
+import { getDate, toastVisibilityTime } from "@/utils/globals";
+import { yupResolver } from "@hookform/resolvers/yup";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { eq, sql } from "drizzle-orm";
-import { Formik } from "formik";
+import { Controller, useForm } from "react-hook-form";
 import { Modal, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Toast from "react-native-toast-message";
 import Icon from "react-native-vector-icons/MaterialIcons";
@@ -23,17 +21,18 @@ const AddWageOrderModal = ({ db, name, refetch }: Props) => {
   const [isVisible, setIsVisible] = useImmer(false);
   const [isDateModalVisible, setIsDateModalVisible] = useImmer(false);
 
-  const initialValues = {
-    name,
-    date: "",
-    less_than_ten: "",
-    ten_or_more: "",
-  };
+  const {
+    control,
+    getValues,
+    setValue,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  const handleSubmit = async (
-    values: Values,
-    { resetForm }: { resetForm: () => void },
-  ) => {
+  const onSubmit = async (values: Values) => {
     const formattedValues = {
       ...values,
       name: values.name.trim(),
@@ -59,7 +58,7 @@ const AddWageOrderModal = ({ db, name, refetch }: Props) => {
       } else {
         await db.insert(wageOrders).values(formattedValues);
         refetch();
-        resetForm();
+        reset();
         setIsVisible(false);
         Toast.show({
           type: "success",
@@ -93,130 +92,152 @@ const AddWageOrderModal = ({ db, name, refetch }: Props) => {
         visible={isVisible}
         onRequestClose={() => setIsVisible(false)}
       >
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleSubmit,
-            handleChange,
-            setFieldValue,
-            setFieldTouched,
-          }) => (
-            <View className="flex-1 items-center justify-center bg-black/40">
-              <View className="w-4/5 rounded-[0.625rem] bg-[#1E90FF] p-4">
-                <View className="flex-row flex-wrap justify-between gap-1">
-                  <View className="w-[49%]">
-                    <Text className="mt-1 font-bold text-white">Name</Text>
-                    <TextInput
-                      className="mt-0.5 rounded-[0.3125rem] bg-white px-2"
-                      placeholder="Enter name"
-                      editable={false}
-                      value={values.name}
-                      onChangeText={handleChange("name")}
-                      onBlur={() => setFieldTouched("name")}
-                    />
-                    {touched.name && errors.name && (
-                      <Text className="mt-1 rounded-md bg-red-500 p-1 text-[0.75rem] text-white">
-                        {errors.name}
-                      </Text>
-                    )}
-                  </View>
+        <View className="flex-1 items-center justify-center bg-black/40">
+          <View className="w-4/5 rounded-[0.625rem] bg-[#1E90FF] p-4">
+            <View className="flex-row flex-wrap justify-between gap-1">
+              <View className="w-[49%]">
+                <Text className="mt-1 font-bold text-white">Name</Text>
 
-                  <View className="w-[49%]">
-                    <Text className="mb-1 text-base font-bold text-white">
-                      Date
-                    </Text>
-                    <TouchableOpacity
-                      className="h-12 flex-row items-center justify-between rounded-md border border-[#ccc] bg-[#fafafa] px-2.5"
-                      onPress={() => setIsDateModalVisible(true)}
-                    >
-                      <Text>{values.date || "Select date"}</Text>
-                      <Icon name="date-range" size={20} color="#555" />
-                    </TouchableOpacity>
-                    {touched.date && errors.date && (
-                      <Text className="mt-1 rounded-md bg-red-500 p-1 text-[0.75rem] text-white">
-                        {errors.date}
-                      </Text>
-                    )}
-                  </View>
-                </View>
-
-                <View>
-                  <Text className="mt-1 font-bold text-white">
-                    Employing 1 to 9 workers:
-                  </Text>
-                  <TextInput
-                    className="mt-0.5 rounded-[0.3125rem] bg-white px-2"
-                    keyboardType="numeric"
-                    placeholder="Enter rate"
-                    value={`${values.less_than_ten}`}
-                    onChangeText={handleChange("less_than_ten")}
-                    onBlur={() => setFieldTouched("less_than_ten")}
-                  />
-                  {touched.less_than_ten && errors.less_than_ten && (
-                    <Text className="mt-1 rounded-md bg-red-500 p-1 text-[0.75rem] text-white">
-                      {errors.less_than_ten}
-                    </Text>
+                <Controller
+                  control={control}
+                  name="name"
+                  render={({ field: { value, onChange, onBlur } }) => (
+                    <>
+                      <TextInput
+                        className="mt-0.5 rounded-[0.3125rem] bg-white px-2"
+                        placeholder="Enter name"
+                        value={value}
+                        onChangeText={onChange}
+                        onBlur={onBlur}
+                      />
+                    </>
                   )}
-                </View>
+                />
 
-                <View>
-                  <Text className="mt-1 font-bold text-white">
-                    Employing 10 workers and above:
+                {errors.name && (
+                  <Text className="mt-1 rounded-md bg-red-500 p-1 text-[0.75rem] text-white">
+                    {errors.name.message}
                   </Text>
-                  <TextInput
-                    className="mt-0.5 rounded-[0.3125rem] bg-white px-2"
-                    keyboardType="numeric"
-                    placeholder="Enter rate"
-                    value={`${values.ten_or_more}`}
-                    onChangeText={handleChange("ten_or_more")}
-                    onBlur={() => setFieldTouched("ten_or_more")}
-                  />
-                  {touched.ten_or_more && errors.ten_or_more && (
-                    <Text className="mt-1 rounded-md bg-red-500 p-1 text-[0.75rem] text-white">
-                      {errors.ten_or_more}
-                    </Text>
-                  )}
-                </View>
-
-                <View className="flex-row justify-end">
-                  <TouchableOpacity
-                    className="mr-2 mt-2.5 rounded bg-white px-2.5 py-[0.3125rem]"
-                    onPress={() => setIsVisible(false)}
-                  >
-                    <Text className="font-bold">Cancel</Text>
-                  </TouchableOpacity>
-
-                  <TouchableOpacity
-                    className="mr-2 mt-2.5 rounded bg-white px-2.5 py-[0.3125rem]"
-                    onPress={() => handleSubmit()}
-                  >
-                    <Text className="font-bold">Add</Text>
-                  </TouchableOpacity>
-                </View>
+                )}
               </View>
 
-              {isDateModalVisible && (
-                <DateTimePicker
-                  value={formatDateValue(values.date)}
-                  mode="date"
-                  onChange={(_, value) => {
-                    setFieldValue(
-                      "date",
-                      (value as Date).toISOString().split("T")[0],
-                    );
-                    setIsDateModalVisible(false);
-                  }}
+              <View className="w-[49%]">
+                <Text className="mb-1 text-base font-bold text-white">
+                  Date
+                </Text>
+
+                <Controller
+                  control={control}
+                  name="date"
+                  render={({ field: { value, onChange, onBlur } }) => (
+                    <>
+                      <TouchableOpacity
+                        className="h-12 flex-row items-center justify-between rounded-md border border-[#ccc] bg-[#fafafa] px-2.5"
+                        onPress={() => setIsDateModalVisible(true)}
+                      >
+                        <Text>{value ? getDate(value) : "Select date"}</Text>
+                        <Icon name="date-range" size={20} color="#555" />
+                      </TouchableOpacity>
+                    </>
+                  )}
                 />
+
+                {errors.date && (
+                  <Text className="mt-1 rounded-md bg-red-500 p-1 text-[0.75rem] text-white">
+                    {errors.date.message}
+                  </Text>
+                )}
+              </View>
+            </View>
+
+            <View>
+              <Text className="mt-1 font-bold text-white">
+                Employing 1 to 9 workers:
+              </Text>
+
+              <Controller
+                control={control}
+                name="less_than_ten"
+                render={({ field: { value, onChange, onBlur } }) => (
+                  <>
+                    <TextInput
+                      className="mt-0.5 rounded-[0.3125rem] bg-white px-2"
+                      keyboardType="numeric"
+                      placeholder="Enter rate"
+                      value={value ? `${value}` : ""}
+                      onChangeText={onChange}
+                      onBlur={onBlur}
+                    />
+                  </>
+                )}
+              />
+
+              {errors.less_than_ten && (
+                <Text className="mt-1 rounded-md bg-red-500 p-1 text-[0.75rem] text-white">
+                  {errors.less_than_ten.message}
+                </Text>
               )}
             </View>
+
+            <View>
+              <Text className="mt-1 font-bold text-white">
+                Employing 10 workers and above:
+              </Text>
+
+              <Controller
+                control={control}
+                name="ten_or_more"
+                render={({ field: { value, onChange, onBlur } }) => (
+                  <>
+                    <TextInput
+                      className="mt-0.5 rounded-[0.3125rem] bg-white px-2"
+                      keyboardType="numeric"
+                      placeholder="Enter rate"
+                      value={value ? `${value}` : ""}
+                      onChangeText={onChange}
+                      onBlur={onBlur}
+                    />
+                  </>
+                )}
+              />
+
+              {errors.ten_or_more && (
+                <Text className="mt-1 rounded-md bg-red-500 p-1 text-[0.75rem] text-white">
+                  {errors.ten_or_more.message}
+                </Text>
+              )}
+            </View>
+
+            <View className="flex-row justify-end">
+              <TouchableOpacity
+                className="mr-2 mt-2.5 rounded bg-white px-2.5 py-[0.3125rem]"
+                onPress={() => setIsVisible(false)}
+              >
+                <Text className="font-bold">Cancel</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                className="mr-2 mt-2.5 rounded bg-white px-2.5 py-[0.3125rem]"
+                onPress={handleSubmit(onSubmit)}
+              >
+                <Text className="font-bold">Add</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {isDateModalVisible && (
+            <DateTimePicker
+              value={getValues("date") || new Date()}
+              mode="date"
+              onChange={(_, value) => {
+                if (value) {
+                  setValue("date", value);
+                  setIsDateModalVisible(false);
+                }
+              }}
+            />
           )}
-        </Formik>
+        </View>
       </Modal>
     </>
   );
