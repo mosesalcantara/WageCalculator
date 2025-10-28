ffc51ca12d682512d29df0705ad7e592285ac00d
MosesAlcantara <mosesalcantara123@gmail.com>
2025-10-28 08:46:13 +0800

    Moved primary color style to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    colors: { primary: "#acb6e2ff" },
-    extend: {},
+    extend: {
+      colors: { primary: "#acb6e2ff" },
+    },
   },
   plugins: [],
 };
