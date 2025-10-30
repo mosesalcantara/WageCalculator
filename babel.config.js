6a6a407992ad6c387ba6c50f021d3e7f8482ba83
MosesAlcantara <mosesalcantara123@gmail.com>
2025-10-30 09:06:20 +0800

    Add worklets plugin to babel config

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,9 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: [["inline-import", { extensions: [".sql"] }]],
+    plugins: [
+      'react-native-worklets/plugin', 
+      ["inline-import", { extensions: [".sql"] }]
+    ],
   };
 };
