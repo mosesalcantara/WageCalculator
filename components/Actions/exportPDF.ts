202ca7db7dd04e9bb2f2c16fb127610115383ac6
MosesAlcantara <mosesalcantara123@gmail.com>
2025-11-12 14:45:51 +0800

    Add save option for pdf export

--- components/Actions/exportPDF.ts
+++ components/Actions/exportPDF.ts
@@ -1,28 +1,81 @@
 import { Establishment } from "@/types/globals";
 import { toastVisibilityTime } from "@/utils/globals";
+import * as FileSystem from "expo-file-system";
 import * as Print from "expo-print";
 import * as Sharing from "expo-sharing";
+import { Alert, Platform } from "react-native";
 import Toast from "react-native-toast-message";
 
+const getFile = async (filename: string, html: string) => {
+  const response = await Print.printToFileAsync({ html, base64: true });
+  const uri = `${response.uri.slice(0, response.uri.lastIndexOf("/"))}/${filename}`;
+
+  await FileSystem.moveAsync({ from: response.uri, to: uri });
+  return { uri, base64: response.base64 };
+};
+
 const exportPDF = async (establishment: Establishment, html: string) => {
-  try {
-    const { uri, base64 } = await Print.printToFileAsync({
-      html,
-      base64: true,
-    });
+  const filename = `${establishment.name}.pdf`;
+  const { uri, base64 } = await getFile(filename, html);
+  const mimeType = "application/pdf";
 
-    console.log(uri);
+  if (uri && base64) {
+    Alert.alert("Export as PDF", "Would you like to Save or Share the file?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Save",
+        onPress: async () => {
+          if (Platform.OS === "android") {
+            try {
+              const permissions =
+                await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
 
-    if (await Sharing.isAvailableAsync()) {
-      await Sharing.shareAsync(uri + `${establishment.name}.pdf`);
-    }
-  } catch (error) {
-    console.error(error);
-    Toast.show({
-      type: "error",
-      text1: "An Error Has Occured. Please Try Again.",
-      visibilityTime: toastVisibilityTime,
-    });
+              if (permissions.granted) {
+                await FileSystem.StorageAccessFramework.createFileAsync(
+                  permissions.directoryUri,
+                  filename,
+                  mimeType,
+                )
+                  .then(async (uri) => {
+                    await FileSystem.writeAsStringAsync(uri, base64, {
+                      encoding: FileSystem.EncodingType.Base64,
+                    });
+                  })
+                  .catch((error) => console.error(error));
+
+                Toast.show({
+                  type: "success",
+                  text1: "File Saved",
+                  visibilityTime: toastVisibilityTime,
+                });
+              }
+            } catch (error) {
+              console.error(error);
+              Toast.show({
+                type: "error",
+                text1: "An Error Has Occured. Please Try Again.",
+              });
+            }
+          } else {
+            Toast.show({
+              type: "info",
+              text1: "Saving Not Supported",
+            });
+          }
+        },
+      },
+      {
+        text: "Share",
+        onPress: async () => {
+          if (await Sharing.isAvailableAsync()) {
+            await Sharing.shareAsync(uri, {
+              mimeType: mimeType,
+              dialogTitle: "Share PDF Report",
+            });
+          }
+        },
+      },
+    ]);
   }
 };
 
